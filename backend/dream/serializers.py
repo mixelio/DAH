f25e7871046e304dd10b9d82804a0dc8f5ee0ac9
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-11-12 14:09:00 +0200

    Add DreamReadSerializer

--- backend/dream/serializers.py
+++ backend/dream/serializers.py
@@ -5,9 +5,27 @@ from dream.models import Dream, Comment
 class DreamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dream
-        fields = ['name', 'description', 'image', 'cost', 'status', 'category', 'location']
+        fields = ['name', 'description', 'image', 'cost', 'category', 'location']
 
     category = serializers.ChoiceField(choices=Dream.Category.choices)
+
+
+class DreamReadSerializer(DreamSerializer):
+    class Meta:
+        model = Dream
+        fields = [
+            'id',
+            'name',
+            'description',
+            'image',
+            'user',
+            'cost',
+            'status',
+            'category',
+            'date_added',
+            'location',
+            'likes'
+        ]
 
 
 class CommentSerializer(serializers.ModelSerializer):
--- backend/dream/views.py
+++ backend/dream/views.py
@@ -1,22 +1,20 @@
 from rest_framework import status, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Comment, Dream
-from .serializers import CommentSerializer, DreamSerializer
+from dream.models import Comment, Dream
+from dream.serializers import CommentSerializer, DreamSerializer, DreamReadSerializer
 
 
 class CommentListCreateView(APIView):
     def get(self, request, dream_id):
-        # Отримуємо всі коментарі до конкретної мрії
         comments = Comment.objects.filter(dream__id=dream_id)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
 
     def post(self, request, dream_id):
-        # Створення нового коментаря
         serializer = CommentSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(dream_id=dream_id, user=request.user)  # автоматичне присвоєння dream і user
+            serializer.save(dream_id=dream_id, user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -36,8 +34,10 @@ class DreamViewSet(viewsets.ModelViewSet):
     queryset = Dream.objects.all()
     serializer_class = DreamSerializer
 
-    # Окремі методи для обробки додаткової логіки (якщо потрібно)
-
     def perform_create(self, serializer):
-        # Автоматично прив'язуємо користувача до мрії під час створення
         serializer.save(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == 'post':
+            return DreamSerializer
+        return DreamReadSerializer
