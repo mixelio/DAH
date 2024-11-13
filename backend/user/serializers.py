9046e12b5bdb7239fcd1838e35bdb53ea75d94c2
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-11-13 21:25:13 +0200

    Add new fields to user serializer

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext as _
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "is_staff")
+        fields = ("id", "email", "password", "is_staff", 'first_name', 'last_name', 'photo')
         read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
