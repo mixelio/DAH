ea1935c671e9d73f3f69ce9c4879a9226b8008ab
Mykhailo Hubko <mixelios@gmail.com>
2024-11-13 14:48:07 +0100

    footer, header and form

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,15 @@
+import {SpeedDial, SpeedDialAction, SpeedDialIcon} from "@mui/material"
+import CloseIcon from '@mui/icons-material/Close';
+import GroupsIcon from '@mui/icons-material/Groups';
 import {NavLink} from "react-router-dom"
+import FacebookIcon from '@mui/icons-material/FacebookTwoTone';
+// import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+const actions = [
+  {id: 1, icon: <FacebookIcon />, name: 'facebook', path: 'https://facebook.com'},
+  {id: 2, icon: <InstagramIcon />, name: 'instagram', path: 'https://instagram.com'}
+]
 
 export const Footer = () => {
   return (
@@ -9,6 +20,29 @@ export const Footer = () => {
             to='/'
             className='navigation__logo'
           />
+          <div className="footer__navigation">
+            <NavLink 
+              to='/aboutus'
+              className='footer__nav-link'
+            >About Us</NavLink>
+            <NavLink 
+              to='/contacts'
+              className='footer__nav-link'
+            >Contacts</NavLink>
+          </div>
+            <SpeedDial
+              ariaLabel="SpeedDial openIcon example"
+              className="footer__speed-dial"
+              icon={<SpeedDialIcon icon={<GroupsIcon />} openIcon={<CloseIcon />} />}
+            >
+              {actions.map((action) => (
+                <SpeedDialAction
+                  icon={action.icon}
+                  tooltipTitle={action.name}
+                  onClick={() => window.open(action.path, '_blank')}
+                />
+              ))}
+            </SpeedDial>
         </div>
       </div>
     </footer>
--- src/components/SingUpInForm/SingUpInForm.tsx
+++ src/components/SingUpInForm/SingUpInForm.tsx
@@ -1,7 +1,12 @@
 import {Visibility, VisibilityOff} from '@mui/icons-material'
 import {Box, Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField} from '@mui/material'
-import {FormEvent, useState} from 'react'
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import {FormEvent, useEffect, useRef, useState} from 'react'
 import sentRegistrateData from '../../utils/axiosClient';
+import {Link} from 'react-router-dom';
 
 export const SingUpInForm = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -13,6 +18,21 @@ export const SingUpInForm = () => {
     email: '',
     password: '',
   });
+
+  const [value, setValue] = useState('1');
+  const [contentHeight, setContentHeight] = useState(0);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      setContentHeight(contentRef.current.clientHeight);
+    }
+  }, [contentHeight, value]);
+
+  const handleChangeTab = (event: React.SyntheticEvent, newValue: string) => {
+    event.preventDefault();
+    setValue(newValue);
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const {name, value} = event.target;
@@ -54,59 +74,162 @@ export const SingUpInForm = () => {
   };
 
   return (
-      <Box
-        component='form'
-        sx={{ '& > :not(style)': { m: 1 } }}
-        noValidate
-        autoComplete="off"
-        className="sing-up-in-form"
-        onSubmit={handleSubmit}
-      >
-        <TextField
-          error={hasError.emailError}
-          name="email"
-          id="outlined-basic"
-          label="E-mail" 
-          variant="outlined"
-          sx={{margin: "0 auto"}}
-          className="sing-up-in-form__login"
-          onChange={handleChange}
-          onBlur={() => {
-            if(!registerData.email.trim()) {
-              setHasError(prevState => ({
-                ...prevState,
-                emailError: true,
-              }))
-            }
+    <div className='sing-up-in-form'>
+      <TabContext value={value}>
+        <Box 
+          sx={{ 
+            borderBottom: 1, 
+            borderColor: 'divider'
           }}
-        />
-        <FormControl sx={{ m: 1 }} variant="outlined" className="sing-up-in-form__password">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            name="password"
-            type={showPassword ? 'text' : 'password'}
-            onChange={handleChange}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label={
-                    showPassword ? 'hide the password' : 'display the password'
+        >
+          <TabList onChange={handleChangeTab} aria-label="lab API tabs example">
+            <Tab label="Sing_Up" value="1" />
+            <Tab label="Sing_In" value="2" />
+          </TabList>
+        </Box>
+        {/*  tabs */}
+        <Box 
+          ref={contentRef}         >
+          <TabPanel value="1">
+            <Box
+              component='form'
+              sx={{ '& > :not(style)': { m: 1 } }}
+              noValidate
+              autoComplete="off"
+              className="sing-up-in-form__sing-up"
+              onSubmit={handleSubmit}
+            >
+              <TextField
+                error={hasError.emailError}
+                name="email"
+                id="outlined-basic"
+                label="E-mail" 
+                variant="outlined"
+                sx={{margin: "0 auto"}}
+                className="sing-up-in-form__login"
+                onChange={handleChange}
+                onBlur={() => {
+                  if(!registerData.email.trim()) {
+                    setHasError(prevState => ({
+                      ...prevState,
+                      emailError: true,
+                    }))
                   }
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  onMouseUp={handleMouseUpPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-        </FormControl>
-        <Button variant="contained" type="submit">Sing Up</Button>
-        <p>{}</p>
-      </Box>
+                }}
+              />
+              <FormControl sx={{ m: 1 }} variant="outlined" className="sing-up-in-form__password">
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  name="password"
+                  type={showPassword ? 'text' : 'password'}
+                  onChange={handleChange}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label={
+                          showPassword ? 'hide the password' : 'display the password'
+                        }
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        onMouseUp={handleMouseUpPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+
+              <FormControl sx={{ m: 1 }} variant="outlined" className="sing-up-in-form__password">
+                <InputLabel htmlFor="outlined-adornment-password">Password repeat</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  name="password_repeat"
+                  type={showPassword ? 'text' : 'password'}
+                  onChange={handleChange}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label={
+                          showPassword ? 'hide the password' : 'display the password'
+                        }
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        onMouseUp={handleMouseUpPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password_repeat"
+                />
+              </FormControl>
+              <Button variant="contained" type="submit">Sing Up</Button>
+              <p>{}</p>
+            </Box>
+          </TabPanel>
+          <TabPanel value="2">
+            <Box
+              component='form'
+              sx={{ '& > :not(style)': { m: 1 } }}
+              noValidate
+              autoComplete="off"
+              className="sing-up-in-form__sing-in"
+              onSubmit={handleSubmit}
+            >
+              <TextField
+                error={hasError.emailError}
+                name="email"
+                id="outlined-basic"
+                label="E-mail" 
+                variant="outlined"
+                sx={{margin: "0 auto"}}
+                className="sing-up-in-form__login"
+                onChange={handleChange}
+                onBlur={() => {
+                  if(!registerData.email.trim()) {
+                    setHasError(prevState => ({
+                      ...prevState,
+                      emailError: true,
+                    }))
+                  }
+                }}
+              />
+              <FormControl sx={{ m: 1 }} variant="outlined" className="sing-up-in-form__password">
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  name="password"
+                  type={showPassword ? 'text' : 'password'}
+                  onChange={handleChange}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label={
+                          showPassword ? 'hide the password' : 'display the password'
+                        }
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        onMouseUp={handleMouseUpPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+                </FormControl>
+                <Button variant="contained" type="submit">Sing In</Button>
+                <Link to='/'>forget password?</Link>
+            </Box>
+          </TabPanel>
+        </Box>
+      </TabContext>
+    </div>
   )
 }
