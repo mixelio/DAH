957f2c44c9f7264f0da1841213cf395f23b2795e
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-11-23 17:17:46 +0200

    Add urls.py for Payment & update requirements.txt

---
+++ backend/payment/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from payment.views import (
+    PaymentViewSet,
+    PaymentSuccessView,
+    PaymentCancelView,
+    PaymentSuccessTempView,
+)
+
+router = DefaultRouter()
+router.register("payment", PaymentViewSet, basename="payment")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path(
+        "payments/success/<str:session_id>/",
+        PaymentSuccessView.as_view(),
+        name="payment-success",
+    ),
+    path("payments/cancel/", PaymentCancelView.as_view(), name="payment-cancel"),
+    path("checkout/", PaymentSuccessTempView.as_view(), name="checkout-success"),
+]
+
+app_name = "payment"
