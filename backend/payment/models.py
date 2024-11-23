2182ce6ea16564859855225270018fb8a06fb1a3
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-11-23 15:51:45 +0200

    Add payment model and register it in admin.py

--- backend/payment/models.py
+++ backend/payment/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "PENDING"
+        PAID = "PAID"
+
+    status = models.CharField(max_length=10, choices=StatusChoices.choices)
+    borrowing_id = models.IntegerField()
+    session_url = models.URLField(max_length=500)
+    session_id = models.CharField(max_length=100)
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.id}: {self.status}"
+
