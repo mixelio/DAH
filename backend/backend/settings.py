2c57ab1b2502fe36f83d4fc024138b0caceebed4
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-11-29 19:47:58 +0200

    Dockerize project

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-^if!g+o_*3hlmu58xlv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
