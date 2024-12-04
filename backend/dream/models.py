3d40289bd795673400be1cf88fdb4fd79343bf4a
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-12-04 17:49:23 +0200

    Fix path in user_image_file_path & dream_image_file_path

--- backend/dream/models.py
+++ backend/dream/models.py
@@ -15,7 +15,7 @@ def dream_image_file_path(instance, filename) -> str:
     _, extension = os.path.splitext(filename)
     filename = f'{slugify(instance.name)}-{uuid.uuid4()}{extension}'
 
-    return os.path.join('uploads/dreams/', filename)
+    return os.path.join('dreams/', filename)
 
 
 class Dream(models.Model):
--- backend/user/models.py
+++ backend/user/models.py
@@ -11,7 +11,7 @@ def user_image_file_path(instance, filename) -> str:
     _, extension = os.path.splitext(filename)
     filename = f'{slugify(instance.email)}-{uuid.uuid4()}{extension}'
 
-    return os.path.join('uploads/users/', filename)
+    return os.path.join('users/', filename)
 
 
 class UserManager(DjangoUserManager):
