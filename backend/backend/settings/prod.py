78a09d74d8aa9b19487da8fce4f8e2d882f39014
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-12-05 19:51:23 +0200

    Set debug False in prod settings

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'dah-production-f4c2.up.railway.app']
 
