ddcba09429ed3808b33350f0f90811a058d20c03
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-12-07 14:42:35 +0200

    Register dream models in admin panel

--- backend/dream/admin.py
+++ backend/dream/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from dream.models import Dream, Comment, Contribution
+
+
+admin.site.register(Dream)
+admin.site.register(Comment)
+admin.site.register(Contribution)
