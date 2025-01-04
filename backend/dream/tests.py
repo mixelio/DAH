10cb3cc2a877ebdc61b6bdc5f38e1c4036033732
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-01-04 19:43:32 +0200

    Add tests for dream

--- backend/dream/tests.py
+++ backend/dream/tests.py
@@ -1,5 +1,10 @@
+import tempfile
+
+from PIL import Image
+from cloudinary.uploader import destroy
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from rest_framework import status
 from rest_framework.test import APIClient
 
 from dream.models import Dream
@@ -7,7 +12,7 @@ from dream.models import Dream
 
 def sample_user(**params):
     defaults = {
-        'email': 'test_email',
+        'email': 'test_email@test.com',
         'password': 'test_password1',
     }
     defaults.update(params)
@@ -29,10 +34,7 @@ def sample_dream(**params):
 class CommentTests(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.user = get_user_model().objects.create_user(
-            "test@test.com",
-            "testpass",
-        )
+        self.user = sample_user(email='mail@com')
         self.client.force_authenticate(self.user)
         self.dream = sample_dream(user=self.user)
 
@@ -54,3 +56,51 @@ class CommentTests(TestCase):
 
 class DreamTests(TestCase):
     def setUp(self):
+        self.client = APIClient()
+        self.user = sample_user(email='mail@com')
+        self.client.force_authenticate(self.user)
+        self.dream = sample_dream(user=self.user)
+
+
+    def test_connection(self):
+        list_response = self.client.get('/api/dream/')
+        retrieve_response = self.client.get(f'/api/dream/{self.dream.id}/')
+        self.assertEqual(list_response.status_code, 200)
+        self.assertEqual(retrieve_response.status_code, 200)
+
+    def test_post_dream(self):
+        url = f'/api/dream/'
+        with tempfile.NamedTemporaryFile(suffix='.jpg') as ntf:
+            img = Image.new('RGB', (10, 10))
+            img.save(ntf, format='JPEG')
+            ntf.seek(0)
+            res = self.client.post(
+                url,
+                {
+                    'name': 'This',
+                    'description': 'This is a test description',
+                    'image': ntf,
+                    'category': Dream.Category.SERVICES,
+                    'location': 'test location',
+                },
+                format='multipart',
+            )
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        dream = Dream.objects.get(name='This')
+        self.assertTrue(dream.image)
+        self.assertEqual(dream.description, 'This is a test description')
+        self.assertEqual(dream.category, Dream.Category.SERVICES)
+        self.assertEqual(dream.location, 'test location')
+        self.assertEqual(dream.user.id, self.user.id)
+
+        image_url = dream.image.url
+        base_url = 'http://res.cloudinary.com/dsylcnoz9/'
+        relative_url = image_url.replace(base_url, '')
+        self.assertEqual(res.data['image'], relative_url)
+
+        if dream.image:
+            public_id = dream.image.public_id
+            if public_id:
+                destroy(public_id)
