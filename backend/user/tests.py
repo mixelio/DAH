3452302dc2c521d30ffdaa6c8eae7c3c8ba4336e
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2025-04-07 19:52:32 +0300

    Add test_favorites_created_on_user_creation

--- backend/user/tests.py
+++ backend/user/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase # noqa F401
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from dream.models import UserFavorites
 
-# Create your tests here.
+User = get_user_model()
+
+
+class UserSignalTest(TestCase):
+    def test_favorites_created_on_user_creation(self):
+        user = User.objects.create(email="test@example.com", password="password123")
+
+        self.assertTrue(UserFavorites.objects.filter(user=user).exists())
